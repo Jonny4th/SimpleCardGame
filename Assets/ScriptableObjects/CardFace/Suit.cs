1301e3970dacf13f9af6f5d6e9aa206352889d64
Tong <jonny4th@gmail.com>
2023-05-10 21:02:43 +0700

    Add card suit and rank

---
+++ Assets/ScriptableObjects/CardFace/Suit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CardFace
+{
+    [CreateAssetMenu(fileName = "NewSuit", menuName = "Suit")]
+    public class Suit : FaceComponent
+    {
+        public int index;
+    }
+}
--- Assets/ScriptableObjects/FaceComponent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewFaceComponent", menuName = "Face Component")]
-public class FaceComponent : ScriptableObject
-{
-    public string Name;
-}
