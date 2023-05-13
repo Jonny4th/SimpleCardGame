fb283ddf7f84b4735fa6f43f586b76b9814fd78f
Tong <jonny4th@gmail.com>
2023-05-13 20:58:53 +0700

    Save and Load Deck to json

--- Assets/ScriptableObjects/CardFace/Rank.cs
+++ Assets/ScriptableObjects/CardFace/Rank.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using Newtonsoft.Json;
+using UnityEngine;
 
 namespace CardFace
 {
     [CreateAssetMenu(fileName = "NewRank", menuName = "Rank")]
+    [JsonObject(MemberSerialization.OptIn)]
     public class Rank : FaceComponent
     {
+        [JsonProperty]
         public int index;
     }
 }
---
+++ Assets/Scripts/Decks/SimpleDeckLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Decks
+{
+    public class SimpleDeckLoader : MonoBehaviour
+    {
+        [SerializeField]
+        string fileName;
+
+        [SerializeField]
+        SimpleDeck deck;
+
+        public void Load()
+        {
+            deck.LoadDeckFromJson(fileName);
+        }
+    }
+}
